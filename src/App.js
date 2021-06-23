06d467c3c7bff8e393bd50ccbe6e1cb58a78e56c
DivyankKargeti <divyankkargeti99@gmail.com>
2021-06-24 02:26:10 +0530

    styling and basic component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ function App() {
   return (
     <div>
       <h1>TODO APP</h1>
+      <div className="container">
+        Add an Item...
+        <br />
+        <input
+          type="text"
+          className="input-text"
+          placeholder="write a todo..."
+        />
+        <button className="add-btn">Add ToDo</button>
+        <div className="list">
+          <ul>
+            <li>
+              <input type="checkbox" />
+              study react js more and more
+              <button className="btn">Delete</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
